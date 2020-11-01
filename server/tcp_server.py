10b7e5b02810183edb144f820d2245281201374e
Mitrofanov AA <mitrofanov-aa@speechpro.com>
2020-11-01 16:27:03 +0300

    added fake answ

--- server/tcp_server.py
+++ server/tcp_server.py
@@ -7,7 +7,7 @@ from PIL import Image
 import time
 
 logging.basicConfig(
-    format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s", datefmt="%m/%d/%Y %H:%M:%S", level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s", datefmt="%m/%d/%Y %H:%M:%S", level=logging.DEBUG,
 )
 logger = logging.getLogger(__name__)
 
@@ -54,18 +54,22 @@ class RapGeneratorServer:
     def get_image(self, con_id=0):
         conn = self.connections[con_id]
         datas = []
+        logger.debug("Waiting image...")
         while True:
             data = conn.recv(1024)
+            logger.debug(f"recived {len(data)} bytes. Continue..")
             datas.extend(data)
             if not data:
-                logger.info("End data recvs")
+                logger.debug("End data recvs")
                 break
+        logger.debug(f"Client sent {len(datas)} bytes")
         if len(datas) == 0:
             return
         img = self.data2image(data)
         return img
 
     def data2image(self, data):
+        logger.debug(f"Convert bytes to image")
         img = Image.open(io.BytesIO(data))
         # img = Image.frombytes(mode=self.args.img_mode,
         #                       size=self.image_size,
@@ -79,15 +83,34 @@ class RapGeneratorServer:
             img.save(name)
         return img
 
+    def response_to_client(self, bytes, con_id=0):
+        logger.debug(f"Send to client {len(bytes)} bytes.")
+        sock = self.connections[con_id]
+        sock.send(bytes)
+        logger.debug(f"Send zero.")
+        sock.send(''.encode())
+        logger.debug("Done")
 
-if __name__ == "__main__":
+
+def main():
     parser = argparse.ArgumentParser()
     RapGeneratorServer.add_args(parser)
     args = parser.parse_args()
     server = RapGeneratorServer(args)
+    logger.info(vars(args))
+    with open('audio/stc-cloud_tts.wav', 'rb') as f:
+        answ = f.read()
+    try:
+        while True:
+            logger.info("Wait to new connection...")
+            server.accept()
+            for img in server.listen():
+                logger.debug("Process image")
+                #TODO real answ
+                server.response_to_client(answ)
+    finally:
+        server.sock.close()
 
-    while True:
-        logger.info("Wait to new connection...")
-        server.accept()
-        for img in server.listen():
-            logger.debug("Process image")
+
+if __name__ == "__main__":
+    main()
